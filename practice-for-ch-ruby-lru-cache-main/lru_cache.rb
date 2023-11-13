b4f06d550c076f1969275e330eaa7f5e50325144
Almoe099 <almoe099@gmail.com>
2023-11-13 14:46:39 -0500

    completed lru cache.rb

--- practice-for-ch-ruby-lru-cache-main/lru_cache.rb
+++ practice-for-ch-ruby-lru-cache-main/lru_cache.rb
@@ -5,18 +5,26 @@ class LRUCache
     end
 
     def count
-      @cache.length
+        @cache.length
     end
 
     def add(el)
-      if @cache.include?(el)
+        if @cache.include?(el)
+            @cache.delete(el)
+            @cache << el
+        elsif count >= size
+            @cache.shift 
+            @cache << el
+        else
+            @cache << el
+        end
     end
 
     def show
-      # shows the items in the cache, with the LRU item first
+        p @cache
+        nil
     end
 
     private
-    # helper methods go here!
 
 end
